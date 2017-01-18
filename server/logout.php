1c0fa2a643bc0d7a3b4b4dc231eb91cdfd83219d
HyunGook Kim <hy1392@koreatech.ac.kr>
2017-01-18 22:01:01 +0900

    로그아웃 수정

--- server/logout.php
+++ server/logout.php
@@ -1,12 +1,5 @@
 <?php
   session_start();
-  ?>
-  <script>
-    alert("<?php echo $_SESSION['name'] ?>님 Kostay에서 로그아웃 하셨습니다.");
-  </script>
-  <?
+  echo $_SESSION['name']."님 Kostay에서 로그아웃 하셨습니다.";
   session_destroy();
-  ?>
-  <script>
-    location.href = "../index.html";
-  </script>
+?>
