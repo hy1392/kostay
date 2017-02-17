6557791e78dc4cf0acad9660c973e36976fbb2d7
HyunGook Kim <hy1392@koreatech.ac.kr>
2017-02-17 15:18:46 +0900

    파일추가 중간저장

--- server/profile.php
+++ server/profile.php
@@ -2,20 +2,6 @@
   include 'dbconnect.php';
   extract($_POST);
 
-  $query = "select * from user_profile where email='".$_SESSION['id']."'";
-  $result = mysqli_query($conn, $query);
-  if(mysqli_num_rows($result)===1){
-    $query = "update user_profile set `description`='".$description."' where email = '".$_SESSION['id']."'";
-    $result = mysqli_query($conn, $query);
-    echo "프로필 등록 성공!";
-  }
-  else{
-    $query = "insert into user_profile (`email`, `img`, `description`) values('".$_SESSION['id']."','user_profile/sample.jpg','".$description."')";
-    $result = mysqli_query($conn, $query);
-    echo "프로필 등록 성공!";
-  }
-
-/*
   if(0<$_FILES['file']['error']){
     echo "업로드할 파일이 잘못되었습니다. 파일을 확인하시고 다시 첨부해 주세요";
   }
@@ -39,7 +25,7 @@
   else{
     $query = "update users set username='".$name."', pwd='".$pwd."'  where email = '".$email."'";
     $result = mysqli_query($conn, $query);
-  }*/
+  }
 
   mysqli_close($conn);
 ?>
--- server/wish_list.php
+++ server/wish_list.php
@@ -1,7 +1,10 @@
 <?php
   include 'dbconnect.php';
   extract($_POST);
-
+  if(!isset($_SESSION['id'])){
+    echo "찜하기 기능을 사용하기 위해서는 먼저 로그인해 주세요.";
+    return;
+  }
   $query = "select * from user_wish_list where email='".$_SESSION['id']."' and house_id='".$target."'";
   $result = mysqli_query($conn, $query);
   if(mysqli_num_rows($result)===1) echo "이미 찜리스트에 추가한 하우스 입니다.";
