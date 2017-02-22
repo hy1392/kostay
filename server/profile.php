5574462ef12bab3e8919407b288007bf769ddff7
HyunGook Kim <hy1392@koreatech.ac.kr>
2017-02-22 15:41:10 +0900

    공실예정일 출력 수정

--- server/profile.php
+++ server/profile.php
@@ -1,29 +1,25 @@
 <?php
   include 'dbconnect.php';
   extract($_POST);
-
-  if(0<$_FILES['file']['error']){
+  $file=$_FILES["file"];
+  $description = $_POST['description'];
+  if(0<$file['error']){
     echo "업로드할 파일이 잘못되었습니다. 파일을 확인하시고 다시 첨부해 주세요";
   }
   else{
-    $filename=$_SESSION['id'];
-    move_uploaded_file($_FILES['file']['tmp_name'], 'user_profile/'.$filename);
+    $filename="test.png";
+    move_uploaded_file($file['tmp_name'], 'user_profile/'.$filename);
   }
   $loc='user_profile/'.$filename;
-  if(isset($condition)){
-    $query = "select * from user_profile where email='".$_SESSION['id']."'";
+
+  $query = "select * from user_profile where email='".$_SESSION['id']."'";
+  $result = mysqli_query($conn, $query);
+  if(mysqli_num_rows($result)===1){
+    $query = "update user_profile set `img`='".$loc."', `description`='".$description."' where email = '".$_SESSION['id']."'";
     $result = mysqli_query($conn, $query);
-    if(mysqli_num_rows($result)===1){
-      $query = "update user_profile set `img`='".$loc."', `description`='".$description."' where email = '".$_SESSION['id']."'";
-      $result = mysqli_query($conn, $query);
-    }
-    else{
-      $query = "insert into user_profile (`email`, `img`, `description`) values('".$_SESSION['id']."','".$loc."','".$description."')";
-      $result = mysqli_query($conn, $query);
-    }
   }
   else{
-    $query = "update users set username='".$name."', pwd='".$pwd."'  where email = '".$email."'";
+    $query = "insert into user_profile (`email`, `img`, `description`) values('".$_SESSION['id']."','".$loc."','".$description."')";
     $result = mysqli_query($conn, $query);
   }
 
--- server/wait_list.php
+++ server/wait_list.php
@@ -7,13 +7,19 @@
     $query = "insert into user_movein_list (`email`,`gender`,`nationality`,`age`,`hp`,`contact_email`,`notify`,`visit_date`,`movein_date`,`skip_visit`,`description`,`house_id`,`bed_id`, `time`)
     values ('".$_SESSION['id']."', '".$gender."', '".$nationality."', '".$age."','".$hp."','".$contact_email."','".$notify."','".$visit_date_modified."','".$movein_date."','".$skip_visit."','".$description."','".$house_id."','".$bed_id."', now());";
     $result = mysqli_query($conn, $query);
-    echo "입주신청을 요청하였습니다.";
+    $countQuery = "select * from user_movein_list where `bed_id`='".$bed_id."'";
+    $countResult=mysqli_query($conn, $countQuery);
+    $row=mysqli_num_rows($countResult);
+    echo "입주신청을 요청하였습니다.(현재 대기 순번 : ".$row."번 째)";
   }
   elseif($condition=="대기"){
     $query = "insert into user_wait_list (`email`,`gender`,`nationality`,`age`,`hp`,`contact_email`,`notify`,`movein_date`,`description`,`house_id`,`bed_id`, `time`)
     values ('".$_SESSION['id']."', '".$gender."', '".$nationality."', '".$age."','".$hp."','".$contact_email."','".$notify."','".$movein_date."','".$description."','".$house_id."','".$bed_id."', now());";
     $result = mysqli_query($conn, $query);
-    echo "대기신청을 요청하였습니다.";
+    $countQuery = "select * from user_wait_list where `bed_id`='".$bed_id."'";
+    $countResult=mysqli_query($conn, $countQuery);
+    $row=mysqli_num_rows($countResult);
+    echo "대기신청을 요청하였습니다.(현재 대기 순번 : ".$row."번 째)";
   }
   elseif($condition=="get"){
 
